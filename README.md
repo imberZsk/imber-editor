3005f0dea3bce85633e913cfcbfbf8f1be1d200e
imber <a1157911285@163.com>
2024-04-28 11:29:44 +0800

    ci: 重新配置了Oauth

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 [编辑器所有功能](https://imber-docs.netlify.app/editor/editor-all/editor-all.html)
 
+## commitlint
+
+build
+chore
+ci
+docs
+feat
+fix
+perf
+refactor
+revert
+style
+test
+
 ## 测试部署
 
 111222333
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
-// import authConfig from '../auth.config'
-// import NextAuth from 'next-auth'
-// export const { auth: middleware } = NextAuth(authConfig)
-export { auth as middleware } from 'auth'
+import authConfig from '../auth.config'
+import NextAuth from 'next-auth'
+export const { auth: middleware } = NextAuth(authConfig)
+// export { auth as middleware } from 'auth'
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']

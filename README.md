98c5292e061858f72a0b78473b4de96c75f7bcc0
imber <shikun.zhang@dreamsmart.com>
2025-07-29 14:59:54 +0800

    chore: 注释掉登录功能

--- README.md
+++ README.md
@@ -2,16 +2,3 @@
 
 [编辑器所有功能](https://imber-docs.netlify.app/editor/editor-all/editor-all.html)
 
-## commitlint
-
-build
-chore
-ci
-docs
-feat
-fix
-perf
-refactor
-revert
-style
-test
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,42 @@
-import { auth, signIn, signOut } from 'auth'
+// import { auth, signIn, signOut } from 'auth'
 
-function SignIn() {
-  return (
-    <form
-      action={async () => {
-        'use server'
-        await signIn('github')
-      }}
-    >
-      <p>You are not logged in</p>
-      <p>环境变量7：{process.env.AUTH_GITHUB_ID}</p>
-      <button type="submit">Sign in with GitHub</button>
-    </form>
-  )
-}
+// function SignIn() {
+//   return (
+//     <form
+//       action={async () => {
+//         'use server'
+//         await signIn('github')
+//       }}
+//     >
+//       <p>You are not logged in</p>
+//       <p>环境变量7：{process.env.AUTH_GITHUB_ID}</p>
+//       <button type="submit">Sign in with GitHub</button>
+//     </form>
+//   )
+// }
 
-function SignOut({ children }: { children: React.ReactNode }) {
-  return (
-    <form
-      action={async () => {
-        'use server'
-        await signOut()
-      }}
-    >
-      <p>{children}</p>
-      <button type="submit">Sign out</button>
-    </form>
-  )
-}
+// function SignOut({ children }: { children: React.ReactNode }) {
+//   return (
+//     <form
+//       action={async () => {
+//         'use server'
+//         await signOut()
+//       }}
+//     >
+//       <p>{children}</p>
+//       <button type="submit">Sign out</button>
+//     </form>
+//   )
+// }
 
 export default async function Page() {
-  let session = await auth()
-  let user = session?.user?.email
+  // let session = await auth()
+  // let user = session?.user?.email
 
   return (
     <section>
       <h1>Home</h1>
-      <div>{user ? <SignOut>{`Welcome ${user}`}</SignOut> : <SignIn />}</div>
+      {/* <div>{user ? <SignOut>{`Welcome ${user}`}</SignOut> : <SignIn />}</div> */}
     </section>
   )
 }
